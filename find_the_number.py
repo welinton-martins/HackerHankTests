f62af8b46b09108e2161985d240f0bccc6aa3cf3
nin <nin@localhost.localdomain>
2024-10-04 15:00:38 -0300

    test 1

---
+++ find_the_number.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'findNumber' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY arr
+#  2. INTEGER k
+#
+
+def findNumber(arr, k):
+    # Write your code here
+     # Check if k is in the array arr
+    if k in arr:
+        return "YES"
+    else:
+        return "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = []
+
+    for _ in range(arr_count):
+        arr_item = int(input().strip())
+        arr.append(arr_item)
+
+    k = int(input().strip())
+
+    result = findNumber(arr, k)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
